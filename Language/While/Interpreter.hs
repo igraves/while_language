7c018e8e4d14be76e0d3dbcfef37f109f2d7eb79
Ian Graves <thegravian@gmail.com>
2012-10-02 18:40:20 -0500

    More cool stuff!

---
+++ Language/While/Interpreter.hs
@@ -0,0 +1,52 @@
+module Language.While.Interpreter where
+import Language.While.Syntax
+import Prelude hiding (LT,GT,EQ)
+
+data Integral a => Env a = Env {
+                                  store :: Var -> AExpr a
+                               }  -- Have fun showing this.
+
+--Don't Judge me
+create_env :: Integral a => [(String, a)] -> Env a
+create_env = create_env_h Env {store = \x -> error "Unbound variable."}
+  where
+      create_env_h e [] = e 
+      create_env_h e ((x,v):xs) = create_env_h (Env (tweak (Var x) 
+                                                           (Const v) 
+                                                           (store e))) xs
+--See the prior comment
+tweak :: Var -> a -> (Var -> a) -> (Var -> a)
+tweak i v f = (\x -> if x == i then v else f x)
+
+update_store i v f = tweak i v f
+
+run :: Integral a => Env a -> Stmt a -> Env a
+run phi Skip = phi
+run phi (If b1 s1 s2)   = if (evalb (store phi) b1)
+                             then run phi s1
+                             else run phi s2
+run phi w@(While b1 s1) = if (evalb (store phi) b1)
+                           then run phi (Seq s1 w)
+                           else phi
+run phi (Seq s1 s2) = run (run phi s1) s2
+
+
+eval ::   Integral a => (Var -> AExpr a) -> AExpr a -> a
+eval _   (Const a)   = a
+eval sto (Ident i)   = eval sto (sto i)
+eval sto (Neg e1)    = negate $ eval sto e1
+eval sto (Add e1 e2) = (eval sto e1) + (eval sto e2)
+eval sto (Sub e1 e2) = (eval sto e1) - (eval sto e2)
+eval sto (Mul e1 e2) = (eval sto e1) * (eval sto e2)
+eval sto (Div e1 e2) = (eval sto e1) `div` (eval sto e2) 
+
+evalb :: Integral a => (Var -> AExpr a) -> BExpr a -> Bool
+evalb sto (LT e1 e2)  =  (eval sto e1) < (eval sto e2)
+evalb sto (LTE e1 e2) =  (eval sto e1) <= (eval sto e2)
+evalb sto (GT e1 e2)  =  (eval sto e1) > (eval sto e2)
+evalb sto (GTE e1 e2) =  (eval sto e1) >= (eval sto e2)
+evalb sto (EQ e1 e2)  =  (eval sto e1) == (eval sto e2)
+evalb sto (NE e1 e2)  =  (eval sto e1) /= (eval sto e2)
+evalb sto (NOT b1)    =  not $ evalb sto b1
+evalb _ F             = False
+evalb _ T             = True
--- Language/While/Syntax.hs
+++ Language/While/Syntax.hs
@@ -1,18 +1,18 @@
 module Language.While.Syntax where
 
 
-data Var = Var String
+data Var = Var String deriving (Show, Eq)
 
-data (Num a, Ord a) => AExpr a =  Add (AExpr a) (AExpr a) 
-                                | Sub (AExpr a) (AExpr a)
-                                | Mul (AExpr a) (AExpr a)
-                                | Div (AExpr a) (AExpr a)
+data Integral a => AExpr a =  Add (AExpr a) (AExpr a) --1st + 2nd
+                                | Sub (AExpr a) (AExpr a) --1st - 2nd
+                                | Mul (AExpr a) (AExpr a) --1st * 2nd
+                                | Div (AExpr a) (AExpr a) --1st/2nd
                                 | Neg (AExpr a)
                                 | Ident Var
-                                | Const a 
+                                | Const a deriving (Show, Eq)
                                 -- Missing is a parens case
 
-data (Num a, Ord a) => BExpr a =  T
+data Integral a => BExpr a =  T
                                 | F
                                 | LT  (AExpr a) (AExpr a)
                                 | LTE (AExpr a) (AExpr a)
@@ -20,13 +20,11 @@ data (Num a, Ord a) => BExpr a =  T
                                 | GTE (AExpr a) (AExpr a)
                                 | EQ  (AExpr a) (AExpr a) 
                                 | NE  (AExpr a) (AExpr a)
-                                | NOT (BExpr a) 
+                                | NOT (BExpr a) deriving (Show, Eq)
                                 -- Missing is a parens case
 
-data (Num a, Ord a) => Stmt a =  Skip
-                              | Assignment Var (AExpr a)
+data Integral a => Stmt a =  Skip
+                              | Assign Var (AExpr a)
                               | If (BExpr a) (Stmt a) (Stmt a) -- Then s1 else s2 respective
                               | While (BExpr a) (Stmt a)
-                              | Call Var (AExpr a)
-                              | Seq (Stmt a) (Stmt a) -- Left, then right
-
+                              | Seq (Stmt a) (Stmt a) deriving (Show, Eq)-- Left, then right
